0ff9f177c9bf96cdc600cefb0fd2827ab21504ef
Charlie McGregor <charlie_mcgregor@outlook.com>
2020-07-09 15:35:15 +1000

    Added typewriter effect to the main section

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,14 +2,26 @@ import React from "react";
 import "./styles.css";
 import strings from "./strings";
 import skylineImg from "../../Assets/Skyline.png";
+import Typewriter from "typewriter-effect";
 
 export default function Main() {
   return (
     <div id="main-wrapper">
       <img src={skylineImg} alt={strings.skylineAlt} id="main-img" />
       <div id="main-overlay-wrapper">
-        <div id="main-quote">"{strings.weMakePeople}"</div>
-        <button id="main-contact-btn">{strings.getInTouch}</button>
+        {/* <div id="main-quote">"{strings.weMakePeople}"</div> */}
+        <div id="main-quote">
+          <Typewriter
+            options={{
+              strings: [strings.weMakePeople],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+        <div id="main-btn-wrapper">
+          <button id="main-contact-btn">{strings.getInTouch}</button>
+        </div>
       </div>
     </div>
   );
